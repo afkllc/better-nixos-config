361c14454e77d2a0e7ed8d4413b9a2a322e12418
iLikeToCode <108116949+iLikeToCode@users.noreply.github.com>
2026-02-04 13:18:52 +0000

    Add buildMachines configuration to hydra.nix

--- machines/hydra.nix
+++ machines/hydra.nix
@@ -5,9 +5,17 @@
     enable = true;
     hydraURL = "http://localhost:3000";
     notificationSender = "hydra@localhost";
-    buildMachinesFiles = [];
     useSubstitutes = true;
   };
+
+  nix.buildMachines = [
+    { hostName = "localhost";
+      protocol = null;
+      system = "x86_64-linux";
+      supportedFeatures = ["kvm" "nixos-test" "big-parallel" "benchmark"];
+      maxJobs = 1;
+    }
+  ];
 
   nix.settings.extra-experimental-features = "nix-command flakes";
   boot.binfmt.emulatedSystems = [ "aarch64-linux" ];

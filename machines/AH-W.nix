48e4f59c3cd05bd19886abb99706a084a0e7c49a
iLikeToCode <archie@archiesbytes.xyz>
2026-02-08 13:37:41 +0000

    disable sleep for desktop

--- machines/AH-W.nix
+++ machines/AH-W.nix
@@ -7,6 +7,10 @@
 
   system.autoUpgrade.flake = lib.mkForce "git+https://github.com/iLikeToCode/nixos-config#ah-w";
 
+  systemd.targets.sleep.enable = false;
+  systemd.targets.suspend.enable = false;
+  systemd.targets.hibernate.enable = false;
+  systemd.targets.hybrid-sleep.enable = false;
 
   environment.systemPackages = with pkgs; [
     self.packages.x86_64-linux.webots

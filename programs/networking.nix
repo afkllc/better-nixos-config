d5d9bd7c269a95a2a161a6cbd8872859a08cb00c
iLikeToCode <archie@archiesbytes.xyz>
2026-02-08 13:27:35 +0000

    xrdp

--- programs/networking.nix
+++ programs/networking.nix
@@ -6,4 +6,7 @@
   networking.resolvconf.enable = true;
   networking.firewall.trustedInterfaces = [ "tailscale0" ];
   networking.firewall.checkReversePath = "loose";
+
+  services.xrdp.enable = true;
+  services.xrdp.defaultWindowManager = "startplasma-x11";
 }

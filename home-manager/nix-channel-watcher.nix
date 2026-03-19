c7719ab2c20aa6921aa9a05a2829b89adaa97368
iLikeToCode <archie@archiesbytes.xyz>
2026-03-19 18:52:13 +0000

    seconds -> milliseconds

--- home-manager/nix-channel-watcher.nix
+++ home-manager/nix-channel-watcher.nix
@@ -17,7 +17,7 @@ let
 
     if [ "$UPSTREAM_REV" != "null" ] && [ "$LOCKED_REV" != "$UPSTREAM_REV" ]; then
       ${pkgs.libnotify}/bin/notify-send \
-        --expire-time=15 \
+        --expire-time=10000 \
         --app-name="Nix Flake Watcher" \
         "nixpkgs update available" \
         "locked: $LOCKED_REV → upstream: $UPSTREAM_REV"

0bdabc4cb8c50cd168e85f84aa10017a5634a9ae
iLikeToCode <archie@archiesbytes.xyz>
2026-03-22 14:11:01 +0000

    modify bridges

--- programs/virtualisation.nix
+++ programs/virtualisation.nix
@@ -14,11 +14,11 @@
     };
 
     networking.interfaces.vmbr0 = {
-        ipv4.addresses = [{ address = "192.168.50.5"; prefixLength = 24; }];
+        ipv4.addresses = [{ address = "192.168.10.1"; prefixLength = 24; }];
     };
 
     networking.interfaces.vmbr1 = {
-        ipv4.addresses = [{ address = "192.168.20.5"; prefixLength = 24; }];
+        ipv4.addresses = [{ address = "192.168.20.1"; prefixLength = 24; }];
     };
 
     services.dnsmasq = {
@@ -29,6 +29,12 @@
 
             dhcp-range = [
                 "vmbr0,192.168.10.10,192.168.10.200,12h"
+                "vmbr1,192.168.20.10,192.168.20.200,12h"
+            ];
+
+            dhcp-option = [
+                "interface:vmbr0,option:router,192.168.10.1"
+                "interface:vmbr1,option:router"  # empty = no gateway
             ];
         };
     };
